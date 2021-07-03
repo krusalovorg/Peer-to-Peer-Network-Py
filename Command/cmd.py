4c866179112e96834ddf0526bcbf2e7888a1d9c2
Egor Dudkin <krusalovpro@gmail.com>
2021-07-03 19:34:29 +0400

    Add class cmd

---
+++ Command/cmd.py
@@ -0,0 +1,18 @@
+# Импортируем нужные библеотеки
+
+import time
+
+import _thread
+
+# Код
+
+# Классы
+
+class cmd(object):
+    def _cmd(self):
+        while True:
+            __cmd = input("> ")
+            if __cmd == "stop":
+                print("stopppp")
+    def __init__(self):
+        _thread.start_new_thread(self._cmd, ())
