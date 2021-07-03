56fd2b661414262061a975f8ba9b350fd57808af
Egor Dudkin <krusalovpro@gmail.com>
2021-07-03 19:32:26 +0400

    Add files via upload

--- p2p.py
+++ p2p.py
@@ -1,68 +1,85 @@
 # Импортируем нужные библеотеки
-import socket # Импортируем библеотеку socket для работы с сокетами
+import socket
 
-import _thread # Импортируем библеотеку _thread для работы с потоками
+from Client.client  import *
 
-import json # Импортирую библеотеку json для работы с json
+from Command.cmd  import *
+
+from msvcrt import getch
+
+import os
+
+# Глобальные переменные
+LOCAL = True
+GLOBAL = False
+
+_OTSTUP = 10*" "
 
 # Код
 
-# Классы
+print("\n" * 10)
+print(_OTSTUP + "Network - LOCAL")
+print(_OTSTUP + "          global")
 
-ip = []
+while True:
+    key = ord(getch())
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("\n" * 10)
+    if key == 72:
+        print(_OTSTUP + "Network - LOCAL")
+        print(_OTSTUP + "          global")
+        LOCAL = True
+        GLOBAL = False
+    if key == 80:
+        print(_OTSTUP + "Network - GLOBAL")
+        print(_OTSTUP + "          local")
+        GLOBAL = True
+        LOCAL = False
+    if key == 13:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        break
 
-node = '127.0.0.1', 3030 # Данные об узле
+def connect(hostname, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Создаем сокет
+    sock.bind(('', 0))  # Задаем сокет как клиент
+    sock.settimeout(1)
 
-class Server(object):
-    def user(self):
-        print("Start threard")
-        while True:
-            try:
-                self.data, self.addr = self.sock.recvfrom(1024)  # Читаем сообщения по 1024 байта
-                print(self.data)
-                if not self.data:
-                    break
-                if self.data == b'stop':
-                    self.conn.close()
-            except ConnectionResetError as e:
-                self.addr.close()  # Закрываем соеденение
+    node = hostname, port  # Данные об узле
 
-    def __init__(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Создаем сокет
+    sock.sendto(json.dumps({'q': 'node?'}).encode(), node)  # Отправление тестового сообщения
+    try:
+        data, addr = sock.recvfrom(1024)
 
-        self.sock.bind(('', 3032))  # Выделяем ip адрес и порт для узла соеденений
-        print("P2P Server Started!")
-        while True:
-            try: self.data, self.addr = self.sock.recvfrom(1024) # Принимаем подключение
-            except socket.error:
-                pass
-            print("Connect:", self.addr)
+        data = json.loads(data.decode())
 
-            user_thread = _thread.start_new_thread(self.user, ())
-
-class Client(object):
-    def __init__(self, ip='138.124.186.33'):
-
-        # Инициализация сокета
-
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Создаем сокет
-
-        self.sock.bind(('', 0)) # Задаем сокет как клиент
-
-        self.sock.sendto('hello, world'.encode(),node) # Отправление тестового сообщения
-
-        self.data, self.addr = self.sock.recvfrom(1024)
-
-        self.data = json.loads(self.data.decode())
-
-        print(self.data)
-        if self.data["ip"] == "ip.0":
-            if self.data["ip"] == 'null':
-                Server()
-            else:
-                self.__init__(ip=self.data)
+        if data["a"] == "+node":
+            result = 1
+        if data["a"] == "-node":
+            result = 0
+        sock.close()
+    except socket.error:
+        result = 0
+    return result
+print("\n" * 10)
+if LOCAL:
+    print(_OTSTUP + "RUN LOCAL NETWORK")
+    for i in range(0, 255):
+        res = connect("192.168.1." + str(i), 3030)
+        if res:
+            print("Device found at: ", "192.168.1." + str(i) + ":" + str(3030),"--->   TRUE","["+"-"*int(i/10)+"]",end="\r")
+            break
         else:
-            print("Ваш ip адрес уже зарегестрирован в p2p.network!")
+            print("Not found device at: ", "192.168.1." + str(i) + ":" + str(3030),"["+"-"*int(i/10)+"]",end="\r")
+        res = connect("192.168.0." + str(i), 3030)
+        if res:
+            print("Device found at: ", "192.168.0." + str(i) + ":" + str(3030),"--->   TRUE","["+"-"*int(i/10)+"]",end="\r")
+            break
+        else:
+            print("Not found device at: ", "192.168.0." + str(i) + ":" + str(3030),"["+"-"*int(i/10)+"]",end="\r")
+else:
+    print(_OTSTUP + "GLOBAL NETWORK NOT WORK!")
 
+input()
 if __name__ == '__main__':
+    cmd()
     Client()
