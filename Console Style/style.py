905e6133d4e96b0950249b75bc44abedc7ff7e4a
Egor Dudkin <krusalovpro@gmail.com>
2021-07-11 10:55:29 +0400

    Delete Console Style directory

--- Console Style/style.py
+++
@@ -1,9 +0,0 @@
-# Импортируем нужные библеотеки
-
-import time # Импортируем библетеку time
-
-class CConsole(object):
-    def _print(self,text):
-        for i in range(len(text)):
-            time.sleep(0.1)
-            print(text[i])
